ca58269e8f8094ab6a70f92d18c51f4e5ce2cadb
zekagranit1 <64788935+zekagranit1@users.noreply.github.com>
2023-01-31 16:04:43 +0100

    product controller back

---
+++ TESTING/TESTING/Controllers/ProductController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TESTING.Data;
+using TESTING.Model;
+
+namespace TESTING.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductController : ControllerBase
+    {
+
+        public readonly AppDbContext _context;
+        public ProductController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var products = await _context.Products.ToListAsync();
+            return Ok(products);
+        }
+        [HttpGet]
+        [Route("getbyid")]
+        public async Task<IActionResult> Get(int id)
+        {
+            var Products = await _context.Products.FindAsync(id);
+            return Ok(Products);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Product product)
+        {
+            _context.Products.Add(product);
+            await _context.SaveChangesAsync();
+            return Created($"/getbyid?id={product.Id}", product);
+        }
+
+
+        [HttpPut]
+        public async Task<IActionResult> Update(Product product)
+        {
+            _context.Products.Update(product);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+        [HttpDelete]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            _context.Products.Remove(product);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+
+    }
+}
